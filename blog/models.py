25481b48bb759780a44e9e27622805284fe2f058
Vladimir Titov <tvv-vg@yandex.ru>
2024-05-15 16:57:12 +0300

    модель blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    content = models.TextField(verbose_name='Содержимое')
+    preview = models.ImageField(upload_to='previews/', verbose_name='Изображение', **NULLABLE)
+    published_date = models.DateTimeField(default=timezone.now, verbose_name='Дата публикации')
+    is_published = models.BooleanField(default=True, verbose_name='Признак публикации')
+    views_count = models.IntegerField(default=0, verbose_name='Количество просмотров')
+
+    class Meta:
+        verbose_name = 'Блог'
+        verbose_name_plural = 'Блоги'
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
